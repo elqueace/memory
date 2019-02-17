992aed841a1770ff9197a7bd57bad1274343ab50
queace972 <queace972@gmail.com>
2019-02-17 15:45:06 +0100

    initial commit

---
+++ js/script.original.js
@@ -0,0 +1,179 @@
+function showCongrats();
+{
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementsByTagName("body").innerHTML =
+            this.responseText;
+        }
+    };
+    xhttp.open("GET", "", true);
+    xhttp.send();
+}
+
+const cardsArray = [{
+    'name': 'shell',
+    'img': 'img/blueshell.png',
+  },
+  {
+    'name': 'star',
+    'img': 'img/star.png',
+  },
+  {
+    'name': 'bobomb',
+    'img': 'img/bobomb.png',
+  },
+  {
+    'name': 'mario',
+    'img': 'img/mario.png',
+  },
+  {
+    'name': 'luigi',
+    'img': 'img/luigi.png',
+  },
+  {
+    'name': 'peach',
+    'img': 'img/peach.png',
+  },
+  {
+    'name': '1up',
+    'img': 'img/1up.png',
+  },
+  {
+    'name': 'mushroom',
+    'img': 'img/mushroom.png',
+  },
+  {
+    'name': 'thwomp',
+    'img': 'img/thwomp.png',
+  },
+  {
+    'name': 'bulletbill',
+    'img': 'img/bulletbill.png',
+  },
+  {
+    'name': 'coin',
+    'img': 'img/coin.png',
+  },
+  {
+    'name': 'goomba',
+    'img': 'img/goomba.png',
+  },
+];
+
+//shuffle the cards in array
+const gameGrid = cardsArray
+  .concat(cardsArray)
+  .sort(() => 0.5 - Math.random());
+
+let firstGuess = '';
+let secondGuess = '';
+let count = 0;
+let previousTarget = null;
+let delay = 1200;
+let nbMatched = 0;
+
+//get the main game div
+const game = document.getElementById('game');
+
+//create the grid, style it and insert in DOM
+const grid = document.createElement('section');
+grid.setAttribute('class', 'grid');
+game.appendChild(grid);
+
+//create a card for each array item
+gameGrid.forEach(item => {
+  const { name, img } = item;
+
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.dataset.name = name;
+
+  const front = document.createElement('div');
+  front.classList.add('front');
+
+  const back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = `url(${img})`;
+
+  grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
+});
+
+
+//add class match for all selected cards
+const match = () => {
+  const selected = document.querySelectorAll('.selected');
+  selected.forEach(div => {
+    div.classList.add('match');
+  });
+};
+
+const resetGuesses = () => {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+  previousTarget = null;
+
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(card => {
+    card.classList.remove('selected');
+  });
+};
+
+let promise = new Promise(function(resolve, reject){
+    
+    grid.addEventListener('click', function(event) {
+
+      const clicked = event.target;
+
+      if (
+        clicked.nodeName === 'SECTION' ||
+        clicked === previousTarget ||
+        clicked.parentNode.classList.contains('selected') ||
+        clicked.parentNode.classList.contains('match')
+      ) {
+        return;
+      }
+
+      if (count < 2) {
+        count++;
+        if (count === 1) {
+          firstGuess = clicked.parentNode.dataset.name;
+          console.log(firstGuess);
+          clicked.parentNode.classList.add('selected');
+        } else {
+          secondGuess = clicked.parentNode.dataset.name;
+          console.log(secondGuess);
+          clicked.parentNode.classList.add('selected');
+        }
+
+        if (firstGuess && secondGuess) {
+          if (firstGuess === secondGuess) {
+            setTimeout(match, delay);
+              nbMatched++;
+                  console.log("nbMatched=" + nbMatched );
+
+          }
+          setTimeout(resetGuesses, delay);
+        }
+        previousTarget = clicked;
+      }
+        //all card matched
+        if(nbMatched == 12)
+        {
+           resolve('all matched');
+        }
+
+    });
+
+});
+
+promise.then(function(){
+    
+    nbMatched = 0;
+    console.log("nbMatchedReset=" + nbMatched );
+    showCongrats();
+});
+
