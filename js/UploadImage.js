bc47f19085c086df82a865348135e803cc40bfbb
AmeenaAZIZ <ameena.aziz9@gmail.com>
2019-03-01 00:36:46 +0100

    récuperation des src des images upload et affichage dans le jeu

--- js/UploadImage.js
+++ js/UploadImage.js
@@ -1,43 +1,29 @@
-window.onload = function(){
+function uploadImages(){ // stocke les images dans localStorage
 
-            //Check File API support
-            if(window.File && window.FileList && window.FileReader)
-            {
-                var filesInput = document.getElementById("files");
+    var imgSrc = []; // tableau avec les sources des images ex: C:/users/ameena/images/nom_image.jpg
+    var files = document.getElementById("my-input").files;
 
-                filesInput.addEventListener("change", function(event){
+    for (var i = 0; i < files.length; i++){
+        imgSrc[i] = files[i].name;
+    }
+    for (var j = 0; j < files.length; j++){
+        var key = "img"+j; // nom de la clé = img + indice => img1, img2, img3...
+        localStorage.setItem(key,imgSrc[j]); // stockage de la source de l'image dans localStorage ex: img1 : img_1.jpg
+    }
+}
 
-                    var files = event.target.files; //FileList object
-                    var output = document.getElementById("cardsArray");
+function loadImages() { // charge les images stockées dans localStorage
+    var a = 0;
+    var id = "img"+a;
+	var nbImages = localStorage.length; // s'il n'y a que les images stockées dans le localStorage
+    for(var k = 0; k < nbImages; k++){
+		id = "img"+k;
+        var vraie_src = localStorage.getItem[k];
+		var x = document.createElement("IMG");
+		x.setAttribute("src", vraie_src);
+		x.setAttribute("id", id);
+        a++;
+		document.body.appendChild(x);
+    }
 
-                    for(var i = 0; i< files.length; i++)
-                    {
-                        var file = files[i];
-
-
-                        var picReader = new FileReader();
-
-                        picReader.addEventListener("load",function(event){
-
-                            var picFile = event.target;
-
-                            var div = document.createElement("div");
-
-                            div.innerHTML = "<img class='thumbnail' src='" + picFile.result + "'" +
-                                "title='" + picFile.name + "'/>";
-
-                            output.insertBefore(div,null);
-
-                        });
-
-                        //Read the image
-                        picReader.readAsDataURL(file);
-                    }
-
-                });
-            }
-            else
-            {
-                console.log("Your browser does not support File API");
-            }
 }
